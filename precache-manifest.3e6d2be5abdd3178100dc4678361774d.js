8eb8415bcdedd5fc38c23aed123937227af2f371
andrew4779 <andrew.kimani@student.moringaschool.com>
2022-06-14 21:28:47 +0300

    Updates

---
+++ precache-manifest.3e6d2be5abdd3178100dc4678361774d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b30c227c4a64fea8adb71f84001f33c8",
+    "url": "/food-seach-app/index.html"
+  },
+  {
+    "revision": "649ef4674e9731741721",
+    "url": "/food-seach-app/static/css/main.682d60cc.chunk.css"
+  },
+  {
+    "revision": "d310b8de8575d8f79fa3",
+    "url": "/food-seach-app/static/js/2.dd023780.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/food-seach-app/static/js/2.dd023780.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "649ef4674e9731741721",
+    "url": "/food-seach-app/static/js/main.616c1cfe.chunk.js"
+  },
+  {
+    "revision": "7849ebaddbdb1af4cbe3",
+    "url": "/food-seach-app/static/js/runtime-main.5c63b833.js"
+  }
+]);
